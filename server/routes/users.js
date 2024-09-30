b2099be158a559e519e2dc21ce99276aea6b45fe
jgorwat <jgorwat@kth.se>
2024-09-30 22:04:20 +0200

    Add example call to db in an endpoint

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../db');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
+/* Example endpoint that
+  1) makes example call to database
+  2) sends placeholder data to client
+*/
+router.get('/', function (req, res, next) {
+  db.create().catch(console.dir);
   res.send('respond with a resource');
 });
 
