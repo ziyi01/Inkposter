09467b2b73fde2e18d2bf143fe25dfb7f3c5aa94
Julia Hallberg <juliahallberg98@outlook.com>
2024-10-15 21:05:35 +0200

    timer on host voting
    Timer on host voting

--- app/src/views/host-voting.tsx
+++ app/src/views/host-voting.tsx
@@ -1,22 +1,29 @@
 import React from 'react';
-import { PlayerCanvas } from '../components/playerInterface';
+import { playerSession } from '../userModel';
 
 interface HostVotingProps {
-  playerCanvas: PlayerCanvas[];
+  playerCanvases: playerSession[];
+  playerNames: {[key: string]: string};
+  timer: React.ReactNode;
 }
 
-const HostVoting: React.FC<HostVotingProps> = ({ playerCanvas }) => {
+const HostVoting: React.FC<HostVotingProps> = ({ playerCanvases, playerNames, timer }) => {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-background text-white">
-      <h1>
+      {/* Timer */}
+      <div className="absolute top-4 right-4 text-2xl font-bold">
+        {timer}
+      </div>
+
+      <h1 className='text-3xl font-bold mb-8'>
         Guess the theme!
       </h1>
-      <h1>
+      <h1 className='text-3xl font-bold mb-8'>
         Vote for the Inkposter!
       </h1>
       {/* Player Canvases */}
       <div className="grid grid-cols-4 gap-6">
-        {playerCanvas.map(canvasData => (
+        {playerCanvases.map(canvasData => (
           <div key={canvasData.playerId} className="flex flex-col items-center">
             <div
               className="h-64 w-36 bg-sky-400 rounded-lg mb-2"
@@ -26,7 +33,7 @@ const HostVoting: React.FC<HostVotingProps> = ({ playerCanvas }) => {
                 backgroundPosition: 'center' 
               }}
             />
-            <p className="text-lg font-semibold">{canvasData.playerName}</p>
+            <p className="text-lg font-semibold">{playerNames[canvasData.playerId]}</p>
           </div>
         ))}
       </div>
