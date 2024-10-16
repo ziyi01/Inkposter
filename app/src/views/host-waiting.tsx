d3525fe549356cfa81501d90875613c8ac4fda5e
Julia Hallberg <juliahallberg98@outlook.com>
2024-10-16 22:21:17 +0200

    small design changes
    design changes on host game, host waiting, player game

--- app/src/views/host-waiting.tsx
+++ app/src/views/host-waiting.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { GiCharacter } from "react-icons/gi";
 import Button from '../components/button';
 import { Player } from '../components/playerInterface';
 
@@ -10,23 +11,25 @@ interface HostWaitingProps {
 
 const HostWaitingView: React.FC<HostWaitingProps> = ({ code, players, handleStartGame }) => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-background text-white text-center">
-      <h1 className="animate-pulse text-2xl font-bold">Waiting for more players to join...</h1>
-      <h2 className="text-xl mt-2">
-        Code to join: <span className="text-sky-400">{code}</span>
-      </h2>
+    <div className="flex flex-col items-center justify-center h-screen bg-background text-white text-center relative">
+      <h1 className="text-xl text-gray-200 mt-2 p-3 bg-gray-800 rounded-md border border-sky-800 shadow-md top-8 absolute">
+        Code to join: <span className="text-sky-400 font-bold">{code}</span>
+      </h1>
+
+      <h1 className="animate-pulse text-3xl font-bold shadow-md">Waiting for more players to join...</h1>
+      
 
       {/* Player Avatars */}
       <div className="grid grid-cols-4 gap-6 my-8">
         {players.length > 0 ? (
           players.map((player) => (
-            <div key={player.playerId} className="flex flex-col items-center">
-              <div className="w-24 h-24 bg-sky-400 rounded-lg"></div>
-              <h4 className="mt-2 text-lg">{player.name}</h4>
+            <div key={player.playerId}>
+              <div className="w-24 h-24 bg-sky-400 rounded-lg shadow-md"><GiCharacter className="w-full h-full"/></div>
+              <h4 className="mt-2 text-lg shadow-md">{player.name}</h4>
             </div>
           ))
         ) : (
-          <p className="col-span-3 text-center">No players joined yet</p>
+          <p className="col-span-4 text-center shadow-md">No players joined yet</p>
         )}
       </div>
 
